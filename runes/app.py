79f5b647d43ea03de169864041794da7d5add64c
Andrii Liashenko <ndrlyashenko@gmail.com>
2020-04-12 18:36:04 +0300

    Enable CORS

--- runes/app.py
+++ runes/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 import json
 
 from runes.twitter.twitter_client import TwitterClient
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/api/v1/tweets/<user_id>")
 def user_timeline(user_id):
